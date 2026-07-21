344f145790c8506940e554be2010fe02601bf2d6
ti3wti <87851578+ti3wti@users.noreply.github.com>
2026-07-20 18:17:10 -0600

    Delete qsl_manager directory

--- qsl_manager/version.py
+++
@@ -1,5 +0,0 @@
-"""Versión de QSL Manager -- un solo lugar para actualizarla."""
-APP_VERSION = "v1.0-beta"
-
-# Repo de GitHub (owner/repo) usado para buscar actualizaciones.
-GITHUB_REPO = "ti3wti/QSL-Manager"
